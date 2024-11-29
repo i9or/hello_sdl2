168bc112beaaf2a8e0d43cd7c5d7ccf6fdb56fde
Igor <dont@bzzr.me>
2024-11-29 21:00:03 +0000

    Add fullscreen

--- main.c
+++ main.c
@@ -13,7 +13,12 @@ int main(int argc, char *argv[]) {
 			0
 			);
 
-	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+	SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
+
+	SDL_SetWindowMouseGrab(window, SDL_TRUE);
+
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	SDL_RenderSetLogicalSize(renderer, 320, 240);
 	SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
 	SDL_RenderClear(renderer);
 	SDL_RenderPresent(renderer);
@@ -30,6 +35,27 @@ int main(int argc, char *argv[]) {
 				quit = true;
 			}
 		}
+
+		// Clear screen with black background
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_RenderClear(renderer);
+
+		// Set draw color to red
+		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+
+		// Create a rectangle
+		SDL_Rect fillRect = {
+			.x = 0,   // X position
+			.y = 0,   // Y position
+			.w = 320,   // Width
+			.h = 240    // Height
+		};
+
+		// Render filled red rectangle
+		SDL_RenderFillRect(renderer, &fillRect);
+
+		// Update screen
+		SDL_RenderPresent(renderer);
 	}
 
 	SDL_DestroyWindow(window);
